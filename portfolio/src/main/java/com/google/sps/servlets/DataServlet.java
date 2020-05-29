04248cbe078a611c48ed808293c37fb2709f486b
Nevin George <nevingeorge@google.com>
2020-05-29 11:02:01 -0500

    Create fully functioning comment feature on contact.html, comments stored in datastore and ordered by timestamp

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,6 +17,9 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
 import java.util.ArrayList;
 import com.google.gson.Gson;
@@ -31,9 +34,12 @@ public class DataServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        long timestamp = System.currentTimeMillis();
         String comment = request.getParameter("comment");
+
         Entity commentEntity = new Entity("comment");
         commentEntity.setProperty("comment", comment);
+        commentEntity.setProperty("timestamp", timestamp);
 
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         datastore.put(commentEntity);
@@ -43,7 +49,16 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      ArrayList<String> commentsHistory = new ArrayList<String>();
+    Query query = new Query("comment").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    ArrayList<String> commentsHistory = new ArrayList<String>();
+    for(Entity entity : results.asIterable()) {
+        String comment = (String) entity.getProperty("comment");
+        commentsHistory.add(comment);
+    }
+
     Gson gson = new Gson();
     String json = gson.toJson(commentsHistory);
     response.setContentType("application/json;");
