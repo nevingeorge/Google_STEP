43d5ed4df4e8c763ea6df1cbbc8a99c405361cc5
Nevin George <nevingeorge@google.com>
2020-06-08 16:44:46 +0000

    Complete the query function in FindMeetingQuery.java. Function finds viable meeting times for an event.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,81 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public final class FindMeetingQuery {
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    long requestDuration = request.getDuration();
+    Collection<String> requestAttendees = request.getAttendees();
+
+    // used in the function intersection
+    HashMap<String, Boolean> requestAttendeesMap = new HashMap<String, Boolean>();
+    Iterator iterator = requestAttendees.iterator(); 
+    while(iterator.hasNext()) {
+        requestAttendeesMap.put((String) iterator.next(), true);
+    }
+
+    // viableTimes contains all of the minutes where meetings can be held
+    boolean[] viableTimes = new boolean[TimeRange.END_OF_DAY+1];
+
+    // initially, all of the minutes are viable
+    for(int i=0;i<viableTimes.length;i++) {
+        viableTimes[i] = true;
+    }
+
+    // consider each event independently
+    iterator = events.iterator(); 
+    while(iterator.hasNext()) {
+        Event event = (Event) iterator.next();
+        TimeRange when = event.getWhen();
+        int eventStart = when.start();
+        int eventDuration = when.duration();
+
+        Collection<String> eventAttendees = event.getAttendees();
+
+        // check if any of the attendees in the request are attending the event
+        if(intersection(requestAttendeesMap, eventAttendees)) {
+            // mark all of the minutes from eventStart to eventStart+eventDuration as false
+            for(int j=eventStart;j<eventStart+eventDuration;j++) {
+                viableTimes[j] = false;
+            }
+        }
+    }
+
+    // perform a linear search over the viable start times
+    // for each potential start time, find the largest contiguous viable time range beginning at that start time
+    // if it's a viable meeting time (i.e. it lasts longer than requestDuration), add it to the output
+    ArrayList<TimeRange> viableMeetingTimes = new ArrayList<TimeRange>();
+    int start = 0;
+    int currentRun = 0;
+    for(int min=0;min<=TimeRange.END_OF_DAY;min++) {
+        if(viableTimes[min]) {
+            currentRun++;
+        }
+        else {
+            if(currentRun >= requestDuration) {
+                viableMeetingTimes.add(TimeRange.fromStartEnd(start, start+currentRun, false));
+            }
+            start = min+1;
+            currentRun = 0;
+        }
+    }
+    if(currentRun >= requestDuration) {
+        viableMeetingTimes.add(TimeRange.fromStartEnd(start, start+currentRun, false));
+    }
+
+    return viableMeetingTimes;
+  }
+
+  public static boolean intersection(HashMap<String, Boolean> requestAttendeesMap, Collection<String> eventAttendees) {
+    Iterator iterator = eventAttendees.iterator();
+    while(iterator.hasNext()) {
+        if(requestAttendeesMap.get(iterator.next()) != null)
+            return true;
+    }
+    return false;
   }
 }
